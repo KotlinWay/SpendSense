c4e70a80d756a2b73559d0c489fbb8daf834e0cb
pikselnsk <pikselnsk@gmail.com>
2024-01-08 15:57:41 +0700

    init strapi server

---
+++ server/src/api/spend-event/routes/spend-event.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * spend-event router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::spend-event.spend-event');
